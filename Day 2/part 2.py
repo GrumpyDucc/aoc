110f8b15930f6a3b38409d84956391e3b313e79d
GrumpyDucc <grumpyduckmail@gmail.com>
2025-06-19 01:23:08 +0200

    day 2 completed

---
+++ Day 2/part 2.py
@@ -0,0 +1,18 @@
+file = open('input', 'r').readlines()
+
+total = 0
+
+for dimension in file:
+    lengths = list(map(int, dimension.split('x')))
+    
+    paper = lengths[0] * lengths [1] * lengths [2]
+
+    largest = lengths[0]
+    for side in lengths:
+        if side > largest: largest = side
+    lengths.remove(largest)
+
+    paper += (lengths[0]+lengths[1])*2
+    total += paper
+
+print(total)
---
+++ Day 2/part1.py
@@ -0,0 +1,21 @@
+input = open('input', 'r').readlines()
+
+total = 0
+
+for dimension in input:
+    lengths = list(map(int, dimension.split('x')))
+    
+    sides = [
+        lengths[0]*lengths[1], 
+        lengths[1]*lengths[2], 
+        lengths[0]*lengths[2]
+    ]
+    
+    smallest = sides[0]
+    total += 2*sides[0]
+    for side in sides[1:]:
+        if side < smallest: smallest = side
+        total += 2*side
+    total += smallest
+
+print(total)
